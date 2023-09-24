69ea67aed87682aaef35bc79cdec1ef9a79dd568
Mateusz <wmateusz993@gmail.com>
2023-09-24 13:43:16 +0200

    ui done with forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header/Header";
+import Form from "./components/Form/Form";
+import Table from "./components/Table/Table";
+interface FormValues {
+	"current-savings": number;
+	"yearly-contribution": number;
+	"expected-return": number;
+	duration: number;
+}
+
+interface YearlyData {
+  year: number;
+  yearlyInterest: number;
+  savingsEndOfYear: number;
+  yearlyContribution: number;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [result,setResult]= useState<YearlyData[]>();
+
+
+  const calculateHandler = (userInput: FormValues) => {
+    const yearlyData: YearlyData[] =[];
+
+    let currentSavings: number = +userInput['current-savings'];
+    const yearlyContribution: number = +userInput['yearly-contribution'];
+    const expectedReturn: number = +userInput['expected-return'] / 100;
+    const duration: number = +userInput['duration'];
+
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest: number = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  setResult(yearlyData);
+  }
+
+	return (
+		<>
+			<Header></Header>
+			<Form onCalculate={calculateHandler}></Form>
+			<Table></Table>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,99 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+
+interface FormValues {
+	"current-savings": number;
+	"yearly-contribution": number;
+	"expected-return": number;
+	duration: number;
+}
+
+const initionalValues: FormValues = {
+	"current-savings": 1000,
+	"yearly-contribution": 120,
+	"expected-return": 5,
+	duration: 10,
+};
+interface MyComponentProps {
+  onCalculate: (userInput: FormValues) => void;
+}
+
+const Form = (props:MyComponentProps)=>{
+	const [values, setValues] = useState<FormValues>(initionalValues);
+
+	const submintHandler = (event: FormEvent<HTMLFormElement>) => {
+
+		event.preventDefault();
+
+     props.onCalculate(values)
+	};
+	const resetHendler = () => {
+		setValues(initionalValues);
+	};
+	const changeHandler = (
+		ipnutname: string,
+		value: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setValues((prevValues) => {
+			return {
+				...prevValues,
+				[ipnutname]: value.target.value,
+			};
+		});
+	};
+
+	return (
+		<form onSubmit={submintHandler} className="form">
+			<div className="input-group">
+				<p>
+					<label htmlFor="current-savings">Current Savings ($)</label>
+					<input
+						onChange={(e) => changeHandler("current-savings", e)}
+						type="number"
+						id="current-savings"
+                        value={values['current-savings']}
+					/>
+				</p>
+				<p>
+					<label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+					<input
+						onChange={(e) => changeHandler("yearly-contribution", e)}
+						type="number"
+						id="yearly-contribution"
+                        value={values['yearly-contribution']}
+					/>
+				</p>
+			</div>
+			<div className="input-group">
+				<p>
+					<label htmlFor="expected-return">
+						Expected Interest (%, per year)
+					</label>
+					<input
+						onChange={(e) => changeHandler("expected-return", e)}
+						type="number"
+						id="expected-return"
+                        value={values['expected-return']}
+					/>
+				</p>
+				<p>
+					<label htmlFor="duration">Investment Duration (years)</label>
+					<input
+						onChange={(e) => changeHandler("duration", e)}
+						type="number"
+						id="duration"
+                        value={values['duration']}
+					/>
+				</p>
+			</div>
+			<p className="actions">
+				<button onClick={resetHendler} type="reset" className="buttonAlt">
+					Reset
+				</button>
+				<button type="submit" className="button">
+					Calculate
+				</button>
+			</p>
+		</form>
+	);
+};
+export default Form;
